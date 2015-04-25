5f5ea0d624dbb8ed863d33cee558ef8e79c67040
zvoltage <cody.knecht@gmail.com>
2015-04-25 11:43:17 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -9,7 +9,7 @@
 
 /*URL for the server logo
 Leave it blank to use text instead*/
-var l_serverImage = "";
+var l_serverImage = "images/image.png";
 
 /*Custom server name configuration
 Leave it blank for automatic server name*/
